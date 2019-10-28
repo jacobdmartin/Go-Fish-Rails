496c954b4ec6f4b2fbe7e7bfd2ee17fefab0cc3b
Jake Martin <jakemartin4christ@gmail.com>
2019-10-28 16:41:25 -0400

    user_login_spec working.

--- spec/system/users_logins_spec.rb
+++ spec/system/users_logins_spec.rb
@@ -1,47 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe "UsersLogins", type: :system do
-
-  def setup
-    @user = users(:michael)
-  end
-
-  # it "login with valid information followed by logout" do
-  #   get login_path
-  #   post login_path, params: { session: { email:    @user.email,
-  #                                         password: 'password' } }
-  #   assert is_logged_in?
-  #   assert_redirected_to @user
-  #   follow_redirect!
-  #   assert_template 'users/show'
-  #   assert_select "a[href=?]", login_path, count: 0
-  #   assert_select "a[href=?]", logout_path
-  #   assert_select "a[href=?]", user_path(@user)
-  #   delete logout_path
-  #   assert_not is_logged_in?
-  #   assert_redirected_to root_url
-  #   follow_redirect!
-  #   assert_select "a[href=?]", login_path
-  #   assert_select "a[href=?]", logout_path,      count: 0
-  #   assert_select "a[href=?]", user_path(@user), count: 0
-  # end
+  let(:jake) { User.create(name: "Jake", password: "password", password_confirmation: "password")}
 
   describe "GET /users_logins" do
-  #   it "login with invalid information" do
-  #     visit login_path
-  #     assert_template 'sessions/new'
-  #     post login_path, params: { session: { email: "", password: "" } }
-  #     assert_template 'sessions/new'
-  #     assert_not flash.empty?
-  #     get root_path
-  #     assert flash.empty?
-  #   end
-
     it "login with invalid information" do
-      visit :login
+      visit login_path
       fill_in 'Name', with: ""
       fill_in 'Password', with: ""
       click_on 'Log in'
+      expect(page).to have_current_path(login_path)
+    end
+  end
+
+  describe "GET /users_logins", chrome: true do
+    it "login with valid information" do
+      visit login_path
+      fill_in 'Name', with: jake.name
+      fill_in 'Password', with: jake.password
+      click_on 'Log in'
+      expect(page).to have_current_path(games_path)
+    end
+  end
+
+  describe 'Authentication' do
+    it 'expects the session to be redirected to the login page because it does not have authorization' do
+      visit games_path
+      expect(page).to have_current_path(login_url, url: true)
     end
   end
 end
