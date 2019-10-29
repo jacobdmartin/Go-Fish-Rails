e05312e919e89445951af70c5db2c3667c4243b6
Jake Martin <jakemartin4christ@gmail.com>
2019-10-29 11:08:14 -0400

    @game.save added to def join.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -28,9 +28,11 @@ class GamesController < ApplicationController
 
   def join
     @game = Game.find(params[:id])
-    player = Player.new(current_user.name)
     game_user = GameUser.create(game: @game, user: current_user)
-    @game.go_fish.add_player(player)
+    game_data = @game.go_fish
+    player = Player.new(current_user.name)
+    game_data.add_player(player)
+    @game.save
     redirect_to game_path(@game)
   end
 
