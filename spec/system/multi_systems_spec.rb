47cc27cf1df648f2dff9fc82c37419962e8d6723
Jake Martin <jakemartin4christ@gmail.com>
2019-10-30 14:45:17 -0400

    created test for expecting a player to have the popup on their screen.

--- spec/system/multi_systems_spec.rb
+++ spec/system/multi_systems_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe 'Game', type: :system do
-  let(:session1) { Capybara::Session.new(:selenium_chrome_headless, app) }
+  let(:session1) { Capybara::Session.new(:selenium_chrome, app) }
   let(:session2) { Capybara::Session.new(:selenium_chrome_headless, app) }
 
   let(:user_fred) { create(:user, name: 'Fred', password: 'examplepassword')}
@@ -58,13 +58,23 @@ RSpec.describe 'Game', type: :system do
     @game.reload
     expect(@game.go_fish.current_player).to eq(@game.go_fish.players[0])
   end
-  
-  it 'expects the end game screen to pop up because the game has finished' do
-    
+
+  describe 'turn', :focus do
+    it 'expects current_player to have popup on their screen' do
+      start_game_with_bill_and_fred
+      session1.refresh
+      expect(session1).to have_css(".overlay")
+    end
   end
   
-  it 'expects the game to automatically delete after all players exit the game' do
+  describe 'the end of the game' do 
+    it 'expects the end game screen to pop up because the game has finished' do
+      
+    end
     
+    it 'expects the game to automatically delete after all players exit the game' do
+      
+    end
   end
 
   def log_in(existing_user, session)
@@ -79,5 +89,15 @@ RSpec.describe 'Game', type: :system do
     session.fill_in 'Name', with: name
     session1.choose('2 Players')
     session.click_on 'Create Game'
+  end
+
+  def start_game_with_bill_and_fred
+    log_in(user_bill, session1)
+    create_game("Current Player Game", session1)
+    @game = Game.find_by(name: "Current Player Game")
+
+    log_in(user_fred, session2)
+    session2.click_on 'Join'
+    @game.reload
   end
 end
