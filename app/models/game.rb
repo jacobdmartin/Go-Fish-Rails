2f48b719f0305b07552a117f7c3f2be9ed52ae38
Jake Martin <jakemartin4christ@gmail.com>
2019-10-30 14:46:45 -0400

    added attribute accessors for players and hand.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,5 @@
 class Game < ApplicationRecord
-  attr_accessor :player_num
+  attr_accessor :player_num, :players, :hand
   serialize :go_fish, GoFish
   validates :name,  presence: true, length: { maximum: 50 }, uniqueness: true
 
